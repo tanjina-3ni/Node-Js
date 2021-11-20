7fe73d02177c6147512bbea840c3fc5cc4ba43b7
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-11-20 16:26:48 +0600

    ejs working well

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
-var bodyParser = require('body-parser');  
+const bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: true })  
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log('Application started and Listening on port 3000');
@@ -14,26 +19,28 @@ app.listen(3000, () => {
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/index.html');
+    res.render(__dirname+'/index.html');
 });
 
 app.get('/registration', (req, res) => {
-    response = {  
-        first_name:req.query.yourname,  
-    };  
-    console.log(response);  
-    //res.end(JSON.stringify(response)); 
+    response = {
+        first_name: req.query.yourname,
+        email : req.query.youremail,
+    };
+    console.log(response);
+    //res.end(JSON.stringify(response));
     res.sendFile(__dirname+'/registration.html');
 });
 
-app.post('/registration2', urlencodedParser, function (req, res) {  
-    // Prepare output in JSON format  
-    response = {  
-        first_name:req.body.firstname,  
-        last_name:req.body.lastname  
-    };  
-    //console.log(response);  
-    res.end(JSON.stringify(response));  
- })  
-
+app.post('/registration/registration2', urlencodedParser, (req, res) => {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.firstname,
+        last_name: req.body.lastname,
+        email : req.body.email,
+    };
+    //console.log(response);
+    //res.end(JSON.stringify(response));
+    res.render(__dirname+'/views/registration2.ejs',response);
+});
 
